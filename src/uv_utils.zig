01d0180d1fbc62e9161e7236b94ebabfc1cd5e34
Javier A. Pollak <javi.po.123@gmail.com>
2024-09-15 10:41:35 -0400

    Split Render out into own file

---
+++ src/uv_utils.zig
@@ -0,0 +1,51 @@
+fn assertBaton(
+    comptime Downgraded: type,
+    comptime UpgradedStruct: type,
+) void {
+    switch (@typeInfo(UpgradedStruct)) {
+        .@"struct" => |s| {
+            if (s.layout != .@"extern") {
+                @compileError("Upgraded type must be a C-ABI struct");
+            }
+            if (s.fields.len < 1) {
+                @compileError("Upgraded type must be a struct with at least one field.");
+            }
+            if (s.fields[0].type != Downgraded) {
+                @compileError("Upgraded type must be a struct with the first field embedding the downgraded type.");
+            }
+        },
+        else => {
+            @compileError("Upgraded type must be a struct");
+        },
+    }
+}
+
+pub fn upgradeBaton(ptr: anytype, comptime UpgradedStruct: type) R: {
+    var info = @typeInfo(@TypeOf(ptr));
+    info.pointer.child = UpgradedStruct;
+    break :R @Type(info);
+} {
+    const Downgraded = switch (@typeInfo(@TypeOf(ptr))) {
+        .pointer => |p| p.child,
+        else => {
+            @compileError("ptr must be a pointer");
+        },
+    };
+    assertBaton(Downgraded, UpgradedStruct);
+    return @ptrCast(ptr);
+}
+
+pub fn downgradeBaton(ptr: anytype, comptime Downgraded: type) R: {
+    var info = @typeInfo(@TypeOf(ptr));
+    info.pointer.child = Downgraded;
+    break :R @Type(info);
+} {
+    const UpgradedStruct = switch (@typeInfo(@TypeOf(ptr))) {
+        .pointer => |p| p.child,
+        else => {
+            @compileError("ptr must be a pointer");
+        },
+    };
+    assertBaton(Downgraded, UpgradedStruct);
+    return @ptrCast(ptr);
+}
